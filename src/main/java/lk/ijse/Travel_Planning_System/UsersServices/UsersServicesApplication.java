aab9a0e009146dc9ce6388d00187dc2ef4e37fef
KasunUmesh <kasunumesh38@gmail.com>
2023-11-02 04:43:06 +0530

    init Project

---
+++ src/main/java/lk/ijse/Travel_Planning_System/UsersServices/UsersServicesApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.Travel_Planning_System.UsersServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersServicesApplication.class, args);
+	}
+
+}
