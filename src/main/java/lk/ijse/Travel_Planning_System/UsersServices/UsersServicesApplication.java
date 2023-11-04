a18535d9149b6ee575b2e74383d487a8728febd4
KasunUmesh <kasunumesh38@gmail.com>
2023-11-05 02:00:49 +0530

    created application.properties

--- src/main/java/lk/ijse/Travel_Planning_System/UsersServices/UsersServicesApplication.java
+++ src/main/java/lk/ijse/Travel_Planning_System/UsersServices/UsersServicesApplication.java
@@ -2,8 +2,9 @@ package lk.ijse.Travel_Planning_System.UsersServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UsersServicesApplication {
 
 	public static void main(String[] args) {
